192a2f0879f531a9bff661c2b4a3719dff7d8d07
HelloIloveGimkit2 <eliaspitre@protonmail.com>
2024-10-15 08:56:48 -0500

    Update bundle.js

--- build/bundle.js
+++ build/bundle.js
@@ -18550,7 +18550,7 @@
 						let angle = getUnsafeWindow().Phaser.Math.Angle.Between(0, 0, vector.x, vector.y);
 						socketManager.sendMessage("FIRE", { angle, x: body.x, y: body.y });
 					},
-					0.000000000000000000000001
+					0.0000000000000000000000000000000000000000000000000001
 				);
 			} else {
 				clearInterval(rapidfireInterval);
